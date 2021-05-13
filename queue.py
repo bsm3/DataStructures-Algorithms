94de43ddbcfdf35fd6acdaa3a7def02adc4e3158
Beysim Mustafov <48449800+bsm3@users.noreply.github.com>
2021-05-13 19:59:31 +0300

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,30 @@
+class Stack:
+	def __init__(self, a):
+		self.a = a
+		self.next = None
+
+class Queue:
+	def __init__(self):
+		self.node = None
+
+	def enque(self, x):
+		if self.node == None:
+			new = Stack(x)
+			self.node = new
+		else:
+			temp = self.node
+			while temp.next != None:
+				temp = temp.next
+			new = Stack(x)
+			temp.next = new
+	def deque(self):
+		if self.node == None:
+			print("Empty")
+			return 
+		self.node = self.node.next
+
+	def puts(self):
+		temp = self.node
+		while temp != None:
+			print(temp.a)
+			temp = temp.next
